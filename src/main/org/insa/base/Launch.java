4c2fbefbad80ee275aa93185ba226fde935db3ab
MaelPlantec <mael.plantec@gmail.com>
2018-04-06 08:42:51 +0200

    launch.java

--- src/main/org/insa/base/Launch.java
+++ src/main/org/insa/base/Launch.java
@@ -12,6 +12,7 @@ import javax.swing.SwingUtilities;
 import org.insa.graph.Graph;
 import org.insa.graph.Path;
 import org.insa.graph.io.BinaryGraphReader;
+import org.insa.graph.io.BinaryPathReader;
 import org.insa.graph.io.GraphReader;
 import org.insa.graph.io.PathReader;
 import org.insa.graphics.drawing.Drawing;
@@ -46,29 +47,29 @@ public class Launch {
     public static void main(String[] args) throws Exception {
 
         // Visit these directory to see the list of available files on Commetud.
-        String mapName = "/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/insa.mapgr";
-        String pathName = "/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Paths/path_fr31insa_rangueil_r2.path";
+        String mapName = "/Users/admin/Desktop/cartes_graphes/insa.mapgr";
+        String pathName = "/Users/admin/Desktop/cartes_graphes/path_fr31insa_rangueil_r2.path";
 
         // Create a graph reader.
         GraphReader reader = new BinaryGraphReader(
                 new DataInputStream(new BufferedInputStream(new FileInputStream(mapName))));
 
-        // TODO: Read the graph.
-        Graph graph = null;
+        // Read the graph.
+        Graph graph = reader.read();
 
         // Create the drawing:
         Drawing drawing = createDrawing();
 
-        // TODO: Draw the graph on the drawing.
+        // Draw the graph on the drawing.
+        drawing.drawGraph(graph);
+        
+        // Create a PathReader.
+        PathReader pathReader = new BinaryPathReader(new DataInputStream(new BufferedInputStream(new FileInputStream(pathName))));
 
-        // TODO: Create a PathReader.
-        PathReader pathReader = null;
+        // Read the path.
+        Path path = pathReader.readPath(graph);
 
-        // TODO: Read the path.
-        Path path = null;
-
-        // TODO: Draw the path.
-
+        // Draw the path.
+        drawing.drawPath(path);
     }
-
 }
