13c706870e784d564618c00c28144d2f96572931
MaelPlantec <mael.plantec@gmail.com>
2018-04-08 16:07:44 +0200

    srtgdnfhvjbk

--- src/main/org/insa/algo/utils/BinaryHeap.java
+++ src/main/org/insa/algo/utils/BinaryHeap.java
@@ -144,7 +144,7 @@ public class BinaryHeap<E extends Comparable<E>> implements PriorityQueue<E> {
 
     @Override
     public void remove(E x) throws ElementNotFoundException {
-        // TODO:
+        // TODO: 
     }
 
     @Override
