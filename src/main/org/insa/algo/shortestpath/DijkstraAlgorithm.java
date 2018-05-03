ba199bcebaefd209da0752f17edb0a345309bd68
MaelPlantec <mael.plantec@gmail.com>
2018-05-03 12:17:34 +0200

    Dijsktra and Label

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,4 +1,8 @@
 package org.insa.algo.shortestpath;
+import org.insa.algo.utils.BinaryHeap;
+import org.insa.graph.Arc;
+import java.util.Iterator;
+
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
@@ -12,20 +16,54 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
         
-        // Initialisation
-        // label.cost[0]=0
-        for(int i = 1; i < data.getGraph().size() ; ++i)
+        BinaryHeap<Label> tas = new BinaryHeap();
+        
+        // Initialisation du tableau de Labels
+        Label[]tableauLabel = new Label [data.getGraph().size()];
+        for(int i = 0; i < data.getGraph().size() ; ++i)
         {
-        		// label.cost[i] = infini
+        		// Utiliser les setters
+        		tableauLabel[i].setCout(Double.POSITIVE_INFINITY);
+        		tableauLabel[i].setPrecedent(null);
+        		tableauLabel[i].setMarquage(false);
+        		tableauLabel[i].setId(i);
         }
         
+        tableauLabel[data.getOrigin().getId()].setCout(0);
+        tas.insert(tableauLabel[data.getOrigin().getId()]);
         
-        
-        
-        
-        
-        
-        
+        boolean testMarquage = true;
+        		
+        while(testMarquage) // Test du marquage des sommets
+        {
+        		testMarquage = false;
+        		for(int i = 0; i < data.getGraph().size() ; ++i)
+        		{
+        			if(!tableauLabel[i].isMarquage())
+        			{
+        				testMarquage = true;
+        			}		
+        		}
+        		
+        		// Extraction du minimum du tas
+        		Label labelMin = tas.deleteMin();
+        		labelMin.setMarquage(true);
+        		
+        		Iterator<Arc> iterateurSuccesseurs = data.getGraph().get(labelMin.getId()).iterator();
+        		while(iterateurSuccesseurs.hasNext()) // Pour tous les successeurs de labelMin
+        		{
+        			Arc arcSuccesseur = iterateurSuccesseurs.next();
+        			Label labelSuccesseur = tableauLabel[arcSuccesseur.getDestination().getId()];
+        			if(!labelSuccesseur.isMarquage())
+        			{
+        				if(labelSuccesseur.getCout() > (labelMin.getCout() + data.getCost(arcSuccesseur)))
+        				{
+        					// Penser à mettre le findMin en father de ceux qu'on a itéré
+        				}
+        			}
+        		}
+        }
+     
         
         return solution;
     }
--- src/main/org/insa/algo/shortestpath/Label.java
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -2,19 +2,29 @@ package org.insa.algo.shortestpath;
 
 import org.insa.graph.*;
 
-public class Label 
+public class Label implements Comparable<Label> 
 {
+	private int id;
 	private double cout;
 	private Node precedent;
 	private boolean marquage;
 	
-	public Label(double init_cout, Node init_precedent, boolean init_marquage)
+	public Label( int init_id, double init_cout, Node init_precedent, boolean init_marquage)
 	{
 		this.cout = init_cout;
 		this.precedent = init_precedent;
 		this.marquage = init_marquage;
+		this.id = init_id;
 	}
-
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int x) {
+		this.id =  x;
+	}
+	
 	public double getCout() {
 		return cout;
 	}
@@ -39,4 +49,23 @@ public class Label
 		this.marquage = marquage;
 	}
 	
+	public int compareTo(Label o)
+	{
+		int result;
+		double i;
+		i =  this.getCout() - o.getCout();
+		if(i<0)
+		{
+			result = -1;
+		}
+		if(i==0)
+		{
+			result = 0;
+		}
+		else
+		{
+			result = 1;
+		}
+		return result;
+	}
 }
