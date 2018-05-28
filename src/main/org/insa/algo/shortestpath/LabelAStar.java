5db951866f2b37f2319a5fab69f16c9f87f1c5ab
MaelPlantec <mael.plantec@gmail.com>
2018-05-28 20:15:12 +0200

    LabelAStar

---
+++ src/main/org/insa/algo/shortestpath/LabelAStar.java
@@ -0,0 +1,86 @@
+package org.insa.algo.shortestpath;
+
+import org.insa.graph.*;
+
+public class LabelAStar implements Comparable<Label> 
+{
+	private int id;
+	private double cout;
+	private Arc arcVersPrecedent;
+	private boolean marquage;
+	private double estimation;
+	
+	public LabelAStar( int init_id, double init_cout, Arc init_precedent, boolean init_marquage, double init_estimation)
+	{
+		this.cout = init_cout;
+		this.estimation = init_estimation;
+		this.arcVersPrecedent = init_precedent;
+		this.marquage = init_marquage;
+		this.id = init_id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int x) {
+		this.id =  x;
+	}
+	
+	public double getCout() {
+		return (this.cout + this.estimation);
+	}
+
+	public void setCout(double cout) {
+		this.cout = cout;
+	}
+	
+	public void setEstimation(double estimation) {
+		this.estimation = estimation;
+	}
+
+	public double getEstimation() {
+		return this.estimation;
+	}
+	
+	public Arc getPrecedent() {
+		return arcVersPrecedent;
+	}
+
+	public void setPrecedent(Arc precedent) {
+		this.arcVersPrecedent = precedent;
+	}
+
+	public boolean isMarquage() {
+		return marquage;
+	}
+
+	public void setMarquage(boolean marquage) {
+		this.marquage = marquage;
+	}
+	
+	public int compareTo(Label o)
+	{
+		int result;
+		double i;
+		i =  this.getCout() - o.getCout();
+		if(i<0)
+		{
+			result = -1;
+		}
+		else if(i==0)
+		{
+			result = 0;
+		}
+		else
+		{
+			result = 1;
+		}
+		return result;
+	}
+	
+	public String toString()
+	{
+		return "id = " + this.id + "  cout = " +this.cout+ "   estimation = " +this.estimation;
+	}
+}
